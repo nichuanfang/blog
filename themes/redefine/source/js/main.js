e4711513414c49aef286f455a0943c5c058d2b5d
nichuanfang <f18326186224@gmail.com>
2024-11-11 10:23:03 +0800

    autotheme

--- themes/redefine/source/js/main.js
+++ themes/redefine/source/js/main.js
@@ -1,82 +1,80 @@
 /* main function */
-import initUtils from "./utils.js";
-import initTyped from "./plugins/typed.js";
-import initModeToggle from "./tools/lightDarkSwitch.js";
-import initLazyLoad from "./layouts/lazyload.js";
-import initScrollTopBottom from "./tools/scrollTopBottom.js";
-import initLocalSearch from "./tools/localSearch.js";
-import initCopyCode from "./tools/codeBlock.js";
+import initUtils from './utils.js'
+import initTyped from './plugins/typed.js'
+import initModeToggle from './tools/lightDarkSwitch.js'
+import initLazyLoad from './layouts/lazyload.js'
+import initScrollTopBottom from './tools/scrollTopBottom.js'
+import initLocalSearch from './tools/localSearch.js'
+import initCopyCode from './tools/codeBlock.js'
 
 export const main = {
   themeInfo: {
     theme: `Redefine v${theme.version}`,
-    author: "EvanNotFound",
-    repository: "https://github.com/EvanNotFound/hexo-theme-redefine",
+    author: 'EvanNotFound',
+    repository: 'https://github.com/EvanNotFound/hexo-theme-redefine',
   },
-  localStorageKey: "REDEFINE-THEME-STATUS",
+  localStorageKey: 'REDEFINE-THEME-STATUS',
   styleStatus: {
     isExpandPageWidth: false,
-    isDark: theme.colors.default_mode && theme.colors.default_mode === "dark",
+    isDark: theme.colors.default_mode && theme.colors.default_mode === 'dark',
     fontSizeLevel: 0,
     isOpenPageAside: true,
+    autoTheme: true,
   },
   printThemeInfo: () => {
     console.log(
-      `      ______ __  __  ______  __    __  ______                       \r\n     \/\\__  _\/\\ \\_\\ \\\/\\  ___\\\/\\ \"-.\/  \\\/\\  ___\\                      \r\n     \\\/_\/\\ \\\\ \\  __ \\ \\  __\\\\ \\ \\-.\/\\ \\ \\  __\\                      \r\n        \\ \\_\\\\ \\_\\ \\_\\ \\_____\\ \\_\\ \\ \\_\\ \\_____\\                    \r\n         \\\/_\/ \\\/_\/\\\/_\/\\\/_____\/\\\/_\/  \\\/_\/\\\/_____\/                    \r\n                                                               \r\n ______  ______  _____   ______  ______ __  __   __  ______    \r\n\/\\  == \\\/\\  ___\\\/\\  __-.\/\\  ___\\\/\\  ___\/\\ \\\/\\ \"-.\\ \\\/\\  ___\\   \r\n\\ \\  __<\\ \\  __\\\\ \\ \\\/\\ \\ \\  __\\\\ \\  __\\ \\ \\ \\ \\-.  \\ \\  __\\   \r\n \\ \\_\\ \\_\\ \\_____\\ \\____-\\ \\_____\\ \\_\\  \\ \\_\\ \\_\\\\\"\\_\\ \\_____\\ \r\n  \\\/_\/ \/_\/\\\/_____\/\\\/____\/ \\\/_____\/\\\/_\/   \\\/_\/\\\/_\/ \\\/_\/\\\/_____\/\r\n                                                               \r\n  Github: https:\/\/github.com\/EvanNotFound\/hexo-theme-redefine`,
-    ); // console log message
+      `      ______ __  __  ______  __    __  ______                       \r\n     \/\\__  _\/\\ \\_\\ \\\/\\  ___\\\/\\ \"-.\/  \\\/\\  ___\\                      \r\n     \\\/_\/\\ \\\\ \\  __ \\ \\  __\\\\ \\ \\-.\/\\ \\ \\  __\\                      \r\n        \\ \\_\\\\ \\_\\ \\_\\ \\_____\\ \\_\\ \\ \\_\\ \\_____\\                    \r\n         \\\/_\/ \\\/_\/\\\/_\/\\\/_____\/\\\/_\/  \\\/_\/\\\/_____\/                    \r\n                                                               \r\n ______  ______  _____   ______  ______ __  __   __  ______    \r\n\/\\  == \\\/\\  ___\\\/\\  __-.\/\\  ___\\\/\\  ___\/\\ \\\/\\ \"-.\\ \\\/\\  ___\\   \r\n\\ \\  __<\\ \\  __\\\\ \\ \\\/\\ \\ \\  __\\\\ \\  __\\ \\ \\ \\ \\-.  \\ \\  __\\   \r\n \\ \\_\\ \\_\\ \\_____\\ \\____-\\ \\_____\\ \\_\\  \\ \\_\\ \\_\\\\\"\\_\\ \\_____\\ \r\n  \\\/_\/ \/_\/\\\/_____\/\\\/____\/ \\\/_____\/\\\/_\/   \\\/_\/\\\/_\/ \\\/_\/\\\/_____\/\r\n                                                               \r\n  Github: https:\/\/github.com\/EvanNotFound\/hexo-theme-redefine`
+    ) // console log message
   },
   setStyleStatus: () => {
-    localStorage.setItem(
-      main.localStorageKey,
-      JSON.stringify(main.styleStatus),
-    );
+    localStorage.setItem(main.localStorageKey, JSON.stringify(main.styleStatus))
   },
   getStyleStatus: () => {
-    let temp = localStorage.getItem(main.localStorageKey);
+    let temp = localStorage.getItem(main.localStorageKey)
     if (temp) {
-      temp = JSON.parse(temp);
+      temp = JSON.parse(temp)
       for (let key in main.styleStatus) {
-        main.styleStatus[key] = temp[key];
+        main.styleStatus[key] = temp[key]
       }
-      return temp;
+      return temp
     } else {
-      return null;
+      return null
     }
   },
   refresh: () => {
-    initUtils();
-    initModeToggle();
-    initScrollTopBottom();
+    initUtils()
+    initModeToggle()
+    initScrollTopBottom()
     if (
       theme.home_banner.subtitle.text.length !== 0 &&
       location.pathname === config.root
     ) {
-      initTyped("subtitle");
+      initTyped('subtitle')
     }
 
     if (theme.navbar.search.enable === true) {
-      initLocalSearch();
+      initLocalSearch()
     }
 
     if (theme.articles.code_block.copy === true) {
-      initCopyCode();
+      initCopyCode()
     }
 
     if (theme.articles.lazyload === true) {
-      initLazyLoad();
+      initLazyLoad()
     }
   },
-};
-
-export function initMain() {
-  main.printThemeInfo();
-  main.refresh();
 }
 
-document.addEventListener("DOMContentLoaded", initMain);
+export function initMain() {
+  main.printThemeInfo()
+  main.refresh()
+}
+
+document.addEventListener('DOMContentLoaded', initMain)
 
 try {
-  swup.hooks.on("page:view", () => {
-    main.refresh();
-  });
+  swup.hooks.on('page:view', () => {
+    main.refresh()
+  })
 } catch (e) {}
--- themes/redefine/source/js/tools/lightDarkSwitch.js
+++ themes/redefine/source/js/tools/lightDarkSwitch.js
@@ -1,45 +1,45 @@
-import { main } from "../main.js";
+import { main } from '../main.js'
 
-const elementCode = ".mermaid";
+const elementCode = '.mermaid'
 
 const saveOriginalData = function () {
   return new Promise((resolve, reject) => {
     try {
       var els = document.querySelectorAll(elementCode),
-        count = els.length;
+        count = els.length
       els.forEach((element) => {
-        element.setAttribute("data-original-code", element.innerHTML);
-        count--;
+        element.setAttribute('data-original-code', element.innerHTML)
+        count--
         if (count == 0) {
-          resolve();
+          resolve()
         }
-      });
+      })
     } catch (error) {
-      reject(error);
+      reject(error)
     }
-  });
-};
+  })
+}
 
 const resetProcessed = function () {
   return new Promise((resolve, reject) => {
     try {
       var els = document.querySelectorAll(elementCode),
-        count = els.length;
+        count = els.length
       els.forEach((element) => {
-        if (element.getAttribute("data-original-code") != null) {
-          element.removeAttribute("data-processed");
-          element.innerHTML = element.getAttribute("data-original-code");
+        if (element.getAttribute('data-original-code') != null) {
+          element.removeAttribute('data-processed')
+          element.innerHTML = element.getAttribute('data-original-code')
         }
-        count--;
+        count--
         if (count == 0) {
-          resolve();
+          resolve()
         }
-      });
+      })
     } catch (error) {
-      reject(error);
+      reject(error)
     }
-  });
-};
+  })
+}
 export const ModeToggle = {
   modeToggleButton_dom: null,
   iconDom: null,
@@ -48,46 +48,59 @@ export const ModeToggle = {
 
   async mermaidInit(theme) {
     if (window.mermaid) {
-      await resetProcessed();
-      mermaid.initialize({ theme });
-      mermaid.init({ theme }, document.querySelectorAll(elementCode));
+      await resetProcessed()
+      mermaid.initialize({ theme })
+      mermaid.init({ theme }, document.querySelectorAll(elementCode))
     }
   },
 
   enableLightMode() {
-    document.body.classList.remove("dark-mode");
-    document.documentElement.classList.remove("dark");
-    document.body.classList.add("light-mode");
-    document.documentElement.classList.add("light");
-    this.iconDom.className = "fa-regular fa-moon";
-    main.styleStatus.isDark = false;
-    main.setStyleStatus();
-    this.mermaidInit(this.mermaidLightTheme);
-    this.setGiscusTheme();
+    document.body.classList.remove('dark-mode')
+    document.documentElement.classList.remove('dark')
+    document.body.classList.add('light-mode')
+    document.documentElement.classList.add('light')
+    this.iconDom.className = 'fa-regular fa-moon'
+    main.styleStatus.isDark = false
+    main.setStyleStatus()
+    this.mermaidInit(this.mermaidLightTheme)
+    this.setGiscusTheme()
   },
 
   enableDarkMode() {
-    document.body.classList.remove("light-mode");
-    document.documentElement.classList.remove("light");
-    document.body.classList.add("dark-mode");
-    document.documentElement.classList.add("dark");
-    this.iconDom.className = "fa-regular fa-brightness";
-    main.styleStatus.isDark = true;
-    main.setStyleStatus();
-    this.mermaidInit(this.mermaidDarkTheme);
-    this.setGiscusTheme();
+    document.body.classList.remove('light-mode')
+    document.documentElement.classList.remove('light')
+    document.body.classList.add('dark-mode')
+    document.documentElement.classList.add('dark')
+    this.iconDom.className = 'fa-regular fa-brightness'
+    main.styleStatus.isDark = true
+    main.setStyleStatus()
+    this.mermaidInit(this.mermaidDarkTheme)
+    this.setGiscusTheme()
+  },
+  //自适应模式
+  autoMode() {
+    document.body.classList.remove('light-mode')
+    document.documentElement.classList.remove('light')
+    document.body.classList.add('dark-mode')
+    document.documentElement.classList.add('dark')
+    this.iconDom.className = 'fa-regular fa-brightness'
+    //根据时间切换
+    main.styleStatus.isDark = false
+    main.setStyleStatus()
+    this.mermaidInit(this.mermaidDarkTheme)
+    this.setGiscusTheme()
   },
 
   async setGiscusTheme(theme) {
-    if (document.querySelector("#giscus-container")) {
-      let giscusFrame = document.querySelector("iframe.giscus-frame");
+    if (document.querySelector('#giscus-container')) {
+      let giscusFrame = document.querySelector('iframe.giscus-frame')
       while (!giscusFrame) {
-        await new Promise((r) => setTimeout(r, 1000));
-        giscusFrame = document.querySelector("iframe.giscus-frame");
+        await new Promise((r) => setTimeout(r, 1000))
+        giscusFrame = document.querySelector('iframe.giscus-frame')
       }
-      while (giscusFrame.classList.contains("giscus-frame--loading"))
-        await new Promise((r) => setTimeout(r, 1000));
-      theme ??= main.styleStatus.isDark ? "dark" : "light";
+      while (giscusFrame.classList.contains('giscus-frame--loading'))
+        await new Promise((r) => setTimeout(r, 1000))
+      theme ??= main.styleStatus.isDark ? 'dark' : 'light'
       giscusFrame.contentWindow.postMessage(
         {
           giscus: {
@@ -96,70 +109,80 @@ export const ModeToggle = {
             },
           },
         },
-        "https://giscus.app",
-      );
+        'https://giscus.app'
+      )
     }
   },
 
   isDarkPrefersColorScheme() {
     return (
-      window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)")
-    );
+      window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)')
+    )
   },
 
   initModeStatus() {
-    const styleStatus = main.getStyleStatus();
+    const styleStatus = main.getStyleStatus()
+    const authTheme = main.styleStatus.autoTheme
+    //如果打开了自动切换主题 根据系统时间自动切换 优先级最高
 
-    if (styleStatus) {
-      styleStatus.isDark ? this.enableDarkMode() : this.enableLightMode();
+    if (authTheme) {
+      this.autoMode()
     } else {
-      this.isDarkPrefersColorScheme().matches
-        ? this.enableDarkMode()
-        : this.enableLightMode();
+      if (styleStatus) {
+        // todo 如果是支持自动切换主题(用autoTheme标识) 则根据当前时间来切换
+        styleStatus.isDark ? this.enableDarkMode() : this.enableLightMode()
+      } else {
+        //使用系统的主题模式
+        this.isDarkPrefersColorScheme().matches
+          ? this.enableDarkMode()
+          : this.enableLightMode()
+      }
     }
   },
 
   initModeToggleButton() {
-    this.modeToggleButton_dom.addEventListener("click", () => {
-      const isDark = document.body.classList.contains("dark-mode");
-      isDark ? this.enableLightMode() : this.enableDarkMode();
-    });
+    this.modeToggleButton_dom.addEventListener('click', () => {
+      const isDark = document.body.classList.contains('dark-mode')
+      //取消主题自动切换
+      main.styleStatus.autoTheme = false
+      isDark ? this.enableLightMode() : this.enableDarkMode()
+    })
   },
 
   initModeAutoTrigger() {
-    const isDarkMode = this.isDarkPrefersColorScheme();
-    isDarkMode.addEventListener("change", (e) => {
-      e.matches ? this.enableDarkMode() : this.enableLightMode();
-    });
+    const isDarkMode = this.isDarkPrefersColorScheme()
+    isDarkMode.addEventListener('change', (e) => {
+      e.matches ? this.enableDarkMode() : this.enableLightMode()
+    })
   },
 
   async init() {
     this.modeToggleButton_dom = document.querySelector(
-      ".tool-dark-light-toggle",
-    );
-    this.iconDom = document.querySelector(".tool-dark-light-toggle i");
+      '.tool-dark-light-toggle'
+    )
+    this.iconDom = document.querySelector('.tool-dark-light-toggle i')
     this.mermaidLightTheme =
-      typeof theme.mermaid !== "undefined" &&
-      typeof theme.mermaid.style !== "undefined" &&
-      typeof theme.mermaid.style.light !== "undefined"
+      typeof theme.mermaid !== 'undefined' &&
+      typeof theme.mermaid.style !== 'undefined' &&
+      typeof theme.mermaid.style.light !== 'undefined'
         ? theme.mermaid.style.light
-        : "default";
+        : 'default'
     this.mermaidDarkTheme =
-      typeof theme.mermaid !== "undefined" &&
-      typeof theme.mermaid.style !== "undefined" &&
-      typeof theme.mermaid.style.dark !== "undefined"
+      typeof theme.mermaid !== 'undefined' &&
+      typeof theme.mermaid.style !== 'undefined' &&
+      typeof theme.mermaid.style.dark !== 'undefined'
         ? theme.mermaid.style.dark
-        : "dark";
-    this.initModeStatus();
-    this.initModeToggleButton();
-    this.initModeAutoTrigger();
+        : 'dark'
+    this.initModeStatus()
+    this.initModeToggleButton()
+    this.initModeAutoTrigger()
     try {
-      await saveOriginalData().catch(console.error);
+      await saveOriginalData().catch(console.error)
     } catch (error) {}
   },
-};
+}
 
 // Exported function
 export default function initModeToggle() {
-  ModeToggle.init();
+  ModeToggle.init()
 }
